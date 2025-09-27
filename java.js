2d157245addebda43b2ca0f4789704aba96e9f76
Dmytro Serhiienko <conneciones@gmail.com>
2025-09-27 17:54:03 +0300

    update

---
+++ java.js
@@ -0,0 +1,38 @@
+// !==================Modal Sign In
+document
+  .getElementById("open-modal-sign-in")
+  .addEventListener("click", function () {
+    document.getElementById("modal-sign-in").classList.add("is-open");
+  });
+
+document
+  .getElementById("close-modal-sign-in")
+  .addEventListener("click", function () {
+    document.getElementById("modal-sign-in").classList.remove("is-open");
+  });
+
+// !==================Sign Up Modal
+document
+  .getElementById("open-modal-sign-up")
+  .addEventListener("click", function () {
+    document.getElementById("modal-sign-up").classList.add("is-open");
+  });
+
+document
+  .getElementById("close-modal-sign-up")
+  .addEventListener("click", function () {
+    document.getElementById("modal-sign-up").classList.remove("is-open");
+  });
+
+// !==================Sign Up Modal PRODUCT SECTION
+document
+  .getElementById("product-open-modal-sign-up")
+  .addEventListener("click", function () {
+    document.getElementById("modal-sign-up").classList.add("is-open");
+  });
+
+document
+  .getElementById("close-modal-sign-up")
+  .addEventListener("click", function () {
+    document.getElementById("modal-sign-up").classList.remove("is-open");
+  });
