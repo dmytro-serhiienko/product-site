6681af17767a378546c7aa22f6643a639ec7329c
Dmytro Serhiienko <conneciones@gmail.com>
2025-09-27 23:33:01 +0300

    update

--- main.js
+++ main.js
@@ -36,3 +36,21 @@ document
   .addEventListener("click", function () {
     document.getElementById("modal-sign-up").classList.remove("is-open");
   });
+
+// !==================Sign Up Modal PRODUCT SECTION
+document
+  .getElementById("open-modal-sign-in-from-sign-up")
+  .addEventListener("click", function () {
+    document.getElementById("modal-sign-up").classList.remove("is-open");
+    document.getElementById("modal-sign-in").classList.add("is-open");
+  });
+
+// // !================== Variant #2 Sign In Modal from Sign Up
+// const link = document.getElementById("open-modal-sign-in-from-sign-up");
+// if (link) {
+//   link.addEventListener("click", function (e) {
+//     e.preventDefault(); // щоб не стрибало по #
+//     document.getElementById("modal-sign-up").classList.remove("is-open");
+//     document.getElementById("modal-sign-in").classList.add("is-open");
+//   });
+// }
